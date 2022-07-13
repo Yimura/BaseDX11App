6d02517590cae93572b9bd5273a6d21cf9a6233e
Yimura <andreas.maerten@scarlet.be>
2022-07-14 01:03:42 +0200

    feat(MainWindow): Resize and position to take up entire screen

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -5,7 +5,11 @@
 
 void gui::main_window()
 {
-	ImGui::Begin("Hello");
+	const auto io = ImGui::GetIO();
+	ImGui::SetNextWindowSize({ io.DisplaySize.x, io.DisplaySize.y }, ImGuiCond_Always);
+	ImGui::SetNextWindowPos({ 0, 0 }, ImGuiCond_Always);
+
+	ImGui::Begin("Hello", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar);
 
 	if (ImGui::Button("Exit Program"))
 		Renderer::exit();
